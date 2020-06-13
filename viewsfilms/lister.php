d3831b743c0c321df710eb64cae83c743f8e29d8
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-06-13 14:43:09 -0400

    feat: Code du modal converti en html et inclus en php

--- viewsfilms/lister.php
+++ viewsfilms/lister.php
@@ -22,26 +22,10 @@ try {
     echo "Problème de lecture dans la base de données.";
 } finally {
     $reponse .= "</div>";
-    /*$reponse .= "<div id='previewYT'></div>";*/
-
-
-    $reponse .= "<div id=\"modal1\" class=\"modal center\">";
-    $reponse .= "<div class=\"modal-content\">";
-    $reponse .= "<h4 id='test2'>Modal Header</h4>";
-    $reponse .= "<iframe src=\"https://www.youtube.com/embed/nTfqKG--JwQ\" class='iframeYT' allowfullscreen></iframe>";
-    $reponse .= "</div>";
-    $reponse .= "<div class=\"modal-footer\">";
-    $reponse .= "<a href=\"#\" class=\"modal-close waves-effect waves-green btn-flat\">Agree</a>";
-    $reponse .= "</div>";
-    $reponse .= "</div>";
     echo $reponse;
+
+    include 'modal.html';
 }
 mysqli_close($connexion);
 
 include 'footer.html';
-
-/*          <div class="card-action">
-                <a href="#">This is a link</a>
-            </div>
-        </div>
-    </div>*/
